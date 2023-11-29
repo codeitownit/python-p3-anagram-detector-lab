97b1fc6a6ef9e6bed0912758f5248ee106a6a019
Everlyn <gachuieve32@gmail.com>
2023-11-29 09:04:34 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, list):
+        anagrams = []
+        for word in list:
+            if sorted(word) == sorted(self.word):
+                anagrams.append(word)
+        return anagrams
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
